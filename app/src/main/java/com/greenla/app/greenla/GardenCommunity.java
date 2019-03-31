6de12ed76c6c9459442d75755c84a79f48f909b2
Michelle Huntley <michellehuntleys@gmail.com>
2019-03-31 10:47:50 -0700

    debugged UI and app flow

--- app/src/main/java/com/greenla/app/greenla/GardenCommunity.java
+++ app/src/main/java/com/greenla/app/greenla/GardenCommunity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +20,9 @@ public class GardenCommunity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garden_community);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         members = (Button)findViewById(R.id.members);
         members.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/greenla/app/greenla/LoginActivity.java
+++ app/src/main/java/com/greenla/app/greenla/LoginActivity.java
@@ -7,11 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText name;
     private EditText password;
-    private Button login;
+    private Button loginBtn;
     private EditText incorrect;
 
     @Override
@@ -21,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
 
         name = (EditText)findViewById(R.id.etUsername);
         password = (EditText)findViewById(R.id.etPassword);
-        login = (Button)findViewById(R.id.btnLogin);
+        loginBtn = (Button)findViewById(R.id.btnLogin);
         incorrect = (EditText)findViewById(R.id.editText4);
         
         if (getIntent().getSerializableExtra("INCORRECT_LOGIN") != null) {
@@ -31,12 +31,44 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
 
-        login.setOnClickListener(new View.OnClickListener() {
+        loginBtn.setText("    ");
+        loginBtn.setOnClickListener(this);
+
+        /*
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 validate(name.getText().toString(), password.getText().toString());
             }
         });
+        */
+    }
+
+    public void onClick(View v){
+
+        switch(v.getId())
+        {
+            case R.id.btnLogin:
+            {
+                //open login screen
+                Intent i = new Intent(this, MapActivity.class);
+                startActivity(i);
+                break;
+            }
+            case R.id.etUsername:
+            {
+                break;
+            }
+            case R.id.etPassword:
+            {
+                break;
+            }
+            case R.id.editText4:
+            {
+                break;
+            }
+
+        }
     }
 
     private void validate(String username, String pass){
@@ -47,7 +79,7 @@ public class LoginActivity extends AppCompatActivity {
         }
         else {
             //incorrect.setVisibility(View.VISIBLE);
-            Intent intent = new Intent(LoginActivity.this, LoginActivity.class);
+            Intent intent = new Intent(LoginActivity.this, MapActivity.class);
             boolean upIncorrect = true;
             intent.putExtra("INCORRECT_LOGIN", upIncorrect);
             startActivity(intent);
