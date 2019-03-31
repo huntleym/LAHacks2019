c12cc0fa1756abe36d1486004acb0022f4e8b5af
Justin Pyne <jpyne@oxy.edu>
2019-03-30 20:57:48 -0700

    creating members link + gradle changes

--- app/src/main/java/com/greenla/app/greenla/GardenCommunity.java
+++ app/src/main/java/com/greenla/app/greenla/GardenCommunity.java
@@ -1,7 +1,9 @@
 package com.greenla.app.greenla;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GardenCommunity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class GardenCommunity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garden_community);
+        TextView textview = (TextView)findViewById(R.id.members);
+        textview.setPaintFlags(textview.getPaintFlags()|Paint.UNDERLINE_TEXT_FLAG);
     }
 }
